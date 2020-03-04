68cbdcfe6727ab2b1714bd2763dbce03804b58f3
lakshmilokesh94 <61788970+lakshmilokesh94@users.noreply.github.com>
2020-03-04 22:49:32 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+
+var routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'room',
+    component: ScheduleComponent,
+  },
+  { path: '**', component: DashboardComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
